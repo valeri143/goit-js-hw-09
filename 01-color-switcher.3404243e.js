dabe9558acf3e318a38dcb3f9155566704692e6f
valeri143 <117216733+valeri143@users.noreply.github.com>
2023-04-15 17:36:06 +0000

    Deploying to gh-pages from @ valeri143/goit-js-hw-09@a18ecdb402e025991eb4faa03593823b30276a68 🚀

---
+++ 01-color-switcher.3404243e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body"),n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.3404243e.js.map
